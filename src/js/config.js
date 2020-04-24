9f8dc80b82d60243224420f2b8a30b9edec16683
Maxim <mcgrelka@yandex.ru>
2020-04-24 18:03:56 +0300

    last

--- src/js/config.js
+++
@@ -1,5 +0,0 @@
-const isDev = process.env.NODE_ENV === 'development'? 'http://praktikum.tk/cohort9/' : 'https://praktikum.tk/cohort9/';
-export const config = {
-    baseUrl: isDev,
-    tohen: 'ca5c4c0a-bb5a-4022-aec3-744a34352b88'
-};
