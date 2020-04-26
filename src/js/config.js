a4a80562e427cfc9c2a9363602d4c3ecf208d8bb
Maxim <mcgrelka@yandex.ru>
2020-04-26 18:20:00 +0300

    token fix

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 const isDev = process.env.NODE_ENV === 'development'? 'http://praktikum.tk/cohort9/' : 'https://praktikum.tk/cohort9/';
 export const config = {
     baseUrl: isDev,
-    tohen: 'ca5c4c0a-bb5a-4022-aec3-744a34352b88'
+    token: 'ca5c4c0a-bb5a-4022-aec3-744a34352b88'
 };
